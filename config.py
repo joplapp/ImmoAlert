55bc02324be1069bc4df9ca371980797ed215b96
Johannes Plapp <johannes@plapp.de>
2017-03-19 20:24:09 +0800

    changed search request

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
 
     # this URL will be checked every once in a while
     # it can also be the result of some search
-    searchUrl = 'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Bayern/Muenchen/-/3,00-5,00/-/EURO-1300,00-3000,00?enteredFrom=result_list'
+    searchUrl = 'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Umkreissuche/M_fcnchen_2dMaxvorstadt/-/115730/2029316/-/1276002059037/5/3,00-/78,00-/EURO-1200,00-2000,00/-/-/false'
 
     receiver = 'johannes@plapp.de'
 
